7e490d98ab5261979209f46192d54f2e975d2a3a
Allen Cruiz <cruizallen2@gmail.com>
2024-11-17 20:40:14 +0800

    🐞 fix: minor fixes

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'usertype' => 'admin',
+            'phone' => '1234567890',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
--- resources/views/home/video.blade.php
+++ resources/views/home/video.blade.php
@@ -5,8 +5,8 @@
                 <h6 class="section-title text-start text-white text-uppercase mb-3">Luxury Living</h6>
                 <h1 class="text-white mb-4">Virtual tour</h1>
                 <p class="text-white mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                <a href="" class="btn btn-primary py-md-3 px-md-5 me-3">Our Rooms</a>
-                <a href="" class="btn btn-light py-md-3 px-md-5">Book A Room</a>
+                <a href="{{ url('our_rooms') }}" class="btn btn-primary py-md-3 px-md-5 me-3">Our Rooms</a>
+                <a href="{{ url('our_rooms') }}" class="btn btn-light py-md-3 px-md-5">Book A Room</a>
             </div>
         </div>
         <div class="col-md-6">
@@ -27,7 +27,7 @@
             <div class="modal-body">
                 <!-- 16:9 aspect ratio -->
                 <div class="ratio ratio-16x9">
-                    
+
                         allow="autoplay"></iframe>
                 </div>
             </div>
